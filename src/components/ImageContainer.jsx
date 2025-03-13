250b572ba9991c53b7ba019602b357b856324ec7
Tanel Pauls <paulstanel@protonmail.com>
2025-03-13 15:16:58 +0200

    test

--- src/components/ImageContainer.jsx
+++ src/components/ImageContainer.jsx
@@ -5,7 +5,7 @@ const ImageContainer = () => {
   return (
     <>
       <img src="https://www.techdonut.co.uk/sites/default/files/production/image/protecting-data-on-lost-and-stolen-usb-drives-5513519171.jpg"></img>
-      <EventDescription></EventDescription>
+      <EventDescription>asd</EventDescription>
     </>
   );
 };
