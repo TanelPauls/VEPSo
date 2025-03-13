fd106afe773e2aacdc08e46de244742fa37ec12c
Tanel Pauls <paulstanel@protonmail.com>
2025-03-13 14:50:50 +0200

    Installed bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import { useState } from "react";
+import Footer from "./components/Footer.jsx";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <h1>VEPSO</h1>
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <>
+      <button className="btn btn-primary">Bootstrap töötab</button>
+      <div className="bg-warning">Footer</div>
+    </>
+  );
+};
+
+export default Footer;
