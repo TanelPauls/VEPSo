2a97a78e941ba90378725f1c22c9efc8ff9ea466
Tanel Pauls <paulstanel@protonmail.com>
2025-03-18 23:41:53 +0200

    fixing css

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import Footer from "./components/Footer.jsx";
 import "./App.css";
+import Gamewindow from "./components/Gamewindow.jsx";
 
 function App() {
   return (
     <>
       <div className="containerWholeScreen">
         <div className="containerForContent">
-          <div className="containerNotFooter">asd</div>
+          <div className="containerNotFooter">
+            <div className="containerGameWindow">
+              <Gamewindow />
+            </div>
+          </div>
           <div className="containerFooter">
             <Footer />
           </div>
--- src/components/GameWindow.jsx
+++ src/components/GameWindow.jsx
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 import ImageContainer from "./ImageContainer.jsx";
 import ProgressBar from "./ProgressBar.jsx";
-//import "./Gamewindow.css";
+import "./Gamewindow.css";
 
-const GameWindow = () => {
+const Gamewindow = () => {
   const [currentQuestion, setCurrentQuestion] = useState(1);
   const totalQuestions = 7;
 
@@ -12,15 +12,19 @@ const GameWindow = () => {
   };
   return (
     <>
+      <div className="containerForGameScreen">asd</div>
+      <div className="containerForProgressbar">
+        <ProgressBar
+          currentQuestion={currentQuestion}
+          totalQuestions={totalQuestions}
+          onQuestionClick={handleQuestionClick}
+        />
+      </div>
       {/* <div className="fill">
         <ImageContainer currentQuestion={currentQuestion} />
 
         <div>
-          <ProgressBar
-            currentQuestion={currentQuestion}
-            totalQuestions={totalQuestions}
-            onQuestionClick={handleQuestionClick}
-          />
+          
         </div>
       </div> */}
     </>
